31bb9613be69e3494fa423e47fd88eccb9c26fb7
Druv <druvkotwani12@gmail.com>
2023-07-09 20:19:07 +0530

    react5

--- React5/tailwind.config.js
+++ React5/tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: "#5A5959",
+        yellow: "#FFEAAE",
+        "dark-yellow": "#FCCA3F",
+        orange: "#F6820C",
+      },
+    },
   },
   plugins: [],
 };
