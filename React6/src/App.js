4cf57e6742982a2ad3b877e70c9ad11c22317ac1
druvkotwani <druvkotwani12@gmail.com>
2023-09-09 10:50:44 +0530

    sorting added

--- React6/src/App.js
+++ React6/src/App.js
@@ -60,11 +60,33 @@ function Form({ onAddItems }) {
 }
 
 function PackingList({ items, onDeleteItems, onToggleItems }) {
+  const [sortBy, setSortBy] = useState('input')
+
+  let sortedItems;
+
+  if (sortBy === 'input') sortedItems = items;
+  if (sortBy === 'description') sortedItems = items.slice().sort((a, b) => a.description.localeCompare(b.description))
+  if (sortBy === 'packed') sortedItems = items.slice().sort((a, b) => Number(a.packed) - Number(b.packed))
 
   return <div className='list'>
     <ul>
-      {items.map((item) => (<Item key={item.id} item={item} onDeleteItems={onDeleteItems} onToggleItems={onToggleItems} />))}
+      {sortedItems.map((item) => (<Item key={item.id} item={item} onDeleteItems={onDeleteItems} onToggleItems={onToggleItems} />))}
     </ul>
+
+    <div className='actions'>
+      <select value={sortBy} onChange={e => setSortBy(e.target.value)}>
+        <option value='input'>
+          Sort by input order
+        </option>
+        <option value='description'>
+          Sort by description order
+        </option>
+        <option value='packed'>
+          Sort by packed status
+        </option>
+      </select>
+    </div>
+
   </div>
 }
 
