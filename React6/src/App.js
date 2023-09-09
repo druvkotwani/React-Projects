313da9e12bb695077bb8a521f10f9b308d0668f2
druvkotwani <druvkotwani12@gmail.com>
2023-09-09 10:21:56 +0530

    footer

--- React6/src/App.js
+++ React6/src/App.js
@@ -20,7 +20,7 @@ const App = () => {
       <Logo />
       <Form onAddItems={handleAddItem} />
       <PackingList items={items} onDeleteItems={handleDeleteItem} onToggleItems={handleToggleItem} />
-      <Stats />
+      <Stats items={items} />
     </div>
   )
 }
@@ -75,12 +75,20 @@ function Item({ item, onDeleteItems, onToggleItems }) {
     <button onClick={() => onDeleteItems(item.id)}>❌</button>
   </li>
 }
-function Stats() {
+function Stats({ items }) {
+  if (!items.length) return <p className='stats'>
+    <em>
+      Start adding some items to your packing list
+    </em>
+  </p>
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length
+  const percentage = Math.round((numPacked / numItems) * 100)
   return <footer className='stats'>
     <em>
-      🧳 You have X items on your list, and you already packed X (X%)
+      {percentage === 100 ? `You got everything! Ready to go✈️` : `🧳 You have ${numItems} items on your list, and you already packed ${numPacked} (${percentage}%)`}
     </em>
   </footer>
 }
 
-export default App
+export default App 
