9dd7ed2739ec2af2e310a9aa60431a15b52f8637
druvkotwani <druvkotwani12@gmail.com>
2023-09-15 21:43:40 +0530

    form completed

--- React7/src/App.js
+++ React7/src/App.js
@@ -46,6 +46,17 @@ export default function App() {
     setShowAddFriend(false);
   }
 
+  function handleSplitBill(value) {
+    setFriends((friends) =>
+      friends.map((friend) =>
+        friend.id === selectedFriend.id ? { ...friend, balance: friend.balance + value } : friend
+      )
+    )
+
+    setSelectedFriend(null);
+  }
+
+
   return <div className="app">
     <div className="sidebar">
       <FriendsList friends={friends} onSelection={handleSelection} selectedFriend={selectedFriend} />
@@ -53,7 +64,7 @@ export default function App() {
       {showAddFriend && <FormAddFriend onAddFriend={handleAddFriend} />}
       <Button onClick={handleShowAddFriend}>{showAddFriend ? 'Close' : 'Add Friend'}</Button>
     </div>
-    {selectedFriend && <FormSplitBill selectedFriend={selectedFriend} />}
+    {selectedFriend && <FormSplitBill selectedFriend={selectedFriend} onSplitBill={handleSplitBill} />}
   </div >
 }
 
@@ -127,13 +138,22 @@ function FormAddFriend({ onAddFriend }) {
   )
 }
 
-function FormSplitBill({ selectedFriend }) {
+function FormSplitBill({ selectedFriend, onSplitBill }) {
   const [bill, setBill] = useState("");
   const [paidByUser, setPaidByUser] = useState("");
   const paidByFriend = bill ? bill - paidByUser : '';
   const [whoIsPaying, setWhoIsPaying] = useState("user");
 
-  return <form className="form-split-bill">
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    if (!bill || !paidByUser) return;
+
+    onSplitBill(whoIsPaying === 'user' ? paidByFriend : -paidByUser)
+
+  }
+
+  return <form className="form-split-bill" onSubmit={handleSubmit}>
     <h2>Split a bill with {selectedFriend.name}</h2>
 
     <label>💰Bill Value</label>
