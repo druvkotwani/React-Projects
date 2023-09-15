7f8382c5a15737cc28dd1071bf0b2566ee0d0064
druvkotwani <druvkotwani12@gmail.com>
2023-09-15 17:27:22 +0530

    add friend form dynamic

--- React7/src/App.js
+++ React7/src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialFriends = [
   {
     id: 118836,
@@ -19,15 +21,25 @@ const initialFriends = [
   },
 ];
 
+function Button({ children, onClick }) {
+  return <button onClick={onClick} className="button">{children}</button>
+}
+
 export default function App() {
+  const [showAddFriend, setShowAddFriend] = useState(false);
+
+  function handleShowAddFriend() {
+    setShowAddFriend(!showAddFriend);
+  }
+
   return <div className="app">
     <div className="sidebar">
       <FriendsList />
-      <FormAddFriend />
-      <Button>Add Friend</Button>
+      {showAddFriend && <FormAddFriend />}
+      <Button onClick={handleShowAddFriend}>{showAddFriend ? 'Close' : 'Add Friend'}</Button>
     </div>
     <FormSplitBill />
-  </div>
+  </div >
 }
 
 function FriendsList() {
@@ -76,10 +88,6 @@ function FormAddFriend() {
       <Button>Add</Button>
     </form>
   )
-}
-
-function Button({ children }) {
-  return <button className="button">{children}</button>
 }
 
 function FormSplitBill() {
