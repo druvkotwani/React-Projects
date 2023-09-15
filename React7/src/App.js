93694e3cef52171708fb23c693d2970ca5914576
druvkotwani <druvkotwani12@gmail.com>
2023-09-15 17:06:35 +0530

    form added

--- React7/src/App.js
+++ React7/src/App.js
@@ -23,7 +23,10 @@ export default function App() {
   return <div className="app">
     <div className="sidebar">
       <FriendsList />
+      <FormAddFriend />
+      <Button>Add Friend</Button>
     </div>
+    <FormSplitBill />
   </div>
 }
 
@@ -57,6 +60,47 @@ function Friend({ friend }) {
       </p>
     )}
 
-    <button className="button">Select</button>
+    <Button>Select</Button>
   </li>
+}
+
+function FormAddFriend() {
+  return (
+    <form className="form-add-friend">
+      <label>🧑‍🤝‍🧑Friend name</label>
+      <input type="text" />
+
+      <label>🌟Image URL</label>
+      <input type="text" />
+
+      <Button>Add</Button>
+    </form>
+  )
+}
+
+function Button({ children }) {
+  return <button className="button">{children}</button>
+}
+
+function FormSplitBill() {
+  return <form className="form-split-bill">
+    <h2>Split a bill with X</h2>
+
+    <label>💰Bill Value</label>
+    <input type="text" />
+
+    <label>✈️Your expense</label>
+    <input type="text" />
+
+    <label>🧑‍🤝‍🧑X's expense</label>
+    <input type="text" disabled />
+
+    <label>🤑Who is paying the bill</label>
+    <select>
+      <option value='user'>You</option>
+      <option value='friend'>X</option>
+    </select>
+
+    <Button>Split Bill</Button>
+  </form>
 }
