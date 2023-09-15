3032bffae6e3a05f6c2dba6ffd827c03d1e26bb6
druvkotwani <druvkotwani12@gmail.com>
2023-09-15 16:54:17 +0530

    friends added

--- React7/src/App.js
+++ React7/src/App.js
@@ -18,3 +18,45 @@ const initialFriends = [
     balance: 0,
   },
 ];
+
+export default function App() {
+  return <div className="app">
+    <div className="sidebar">
+      <FriendsList />
+    </div>
+  </div>
+}
+
+function FriendsList() {
+  const friends = initialFriends;
+  return <ul>
+    {friends.map((friend) => (
+      <Friend friend={friend} key={friend.id} />
+    ))}
+  </ul>
+}
+
+function Friend({ friend }) {
+  return <li>
+    <img src={friend.image} alt={friend.name} />
+    <h3>{friend.name}</h3>
+
+    {friend.balance < 0 && (
+      <p className="red">
+        You owe {friend.name} {Math.abs(friend.balance)}$
+      </p>
+    )}
+    {friend.balance > 0 && (
+      <p className="green">
+        {friend.name} owes you {Math.abs(friend.balance)}$
+      </p>
+    )}
+    {friend.balance === 0 && (
+      <p>
+        You and {friend.name} are even
+      </p>
+    )}
+
+    <button className="button">Select</button>
+  </li>
+}
