b6569296fa6ccb686f849be97d8485866d5501e9
druvkotwani <druvkotwani12@gmail.com>
2023-09-17 20:39:34 +0530

    prop drilling

--- React8/src/App.js
+++ React8/src/App.js
@@ -52,30 +52,31 @@ const average = (arr) =>
 
 export default function App() {
 
+  const [movies, setMovies] = useState(tempMovieData);
+
   return (
     <>
-      <Navbar />
-
-      <Main />
+      <Navbar movies={movies} />
+      <Main movies={movies} />
     </>
   );
 }
 
-function Navbar() {
+function Navbar({ movies }) {
 
   return (
     <nav className="nav-bar">
       <Logo />
       <Search />
-      <Numresults />
+      <Numresults movies={movies} />
     </nav>
   )
 }
 
-function Numresults() {
+function Numresults({ movies }) {
   return (
     <p className="num-results">
-      Found <strong>x</strong> results
+      Found <strong>{movies.length}</strong> results
     </p>
   )
 }
@@ -103,16 +104,16 @@ function Search() {
   )
 }
 
-function Main() {
+function Main({ movies }) {
   return (
     <main className="main">
-      <ListBox />
+      <ListBox movies={movies} />
       <WatchedBox />
     </main>
   )
 }
 
-function ListBox() {
+function ListBox({ movies }) {
 
   const [isOpen1, setIsOpen1] = useState(true);
 
@@ -124,13 +125,12 @@ function ListBox() {
       >
         {isOpen1 ? "–" : "+"}
       </button>
-      {isOpen1 && <MovieList />}
+      {isOpen1 && <MovieList movies={movies} />}
     </div>
   )
 }
 
-function MovieList() {
-  const [movies, setMovies] = useState(tempMovieData);
+function MovieList({ movies }) {
 
   return (
     <ul className="list">
