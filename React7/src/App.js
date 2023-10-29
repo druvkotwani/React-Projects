e4e461863c68e5e3ea581b44819b90ff65c1a80b
druvkotwani <druvkotwani12@gmail.com>
2023-10-29 20:22:57 +0530

    key added

--- React7/src/App.js
+++ React7/src/App.js
@@ -64,7 +64,7 @@ export default function App() {
       {showAddFriend && <FormAddFriend onAddFriend={handleAddFriend} />}
       <Button onClick={handleShowAddFriend}>{showAddFriend ? 'Close' : 'Add Friend'}</Button>
     </div>
-    {selectedFriend && <FormSplitBill selectedFriend={selectedFriend} onSplitBill={handleSplitBill} />}
+    {selectedFriend && <FormSplitBill selectedFriend={selectedFriend} onSplitBill={handleSplitBill} key={selectedFriend.id} />}
   </div >
 }
 
